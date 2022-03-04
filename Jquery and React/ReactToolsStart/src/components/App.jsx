b08f67661411accb77db77d01ce22876363cfeb9
Nigel Lacewell <nlacewell97@gmail.com>
2022-03-04 09:46:31 -0600

    consolidation

---
+++ Jquery and React/ReactToolsStart/src/components/App.jsx
@@ -0,0 +1,14 @@
+import Contact from "./Contact.jsx";
+function App() {
+  return (
+    <div>
+      <Contact
+        name="Leroy Brown"
+        telNumber="(123)-456-7890"
+        email="LB35@yahoo.com"
+      />
+    </div>
+  );
+}
+
+export default App;
